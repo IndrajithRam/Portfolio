e0c4d3e1a307fd262ddcdfc1775d0cb985db2ec5
haneefmhmmd <haneefmhmmd@gmail.com>
2024-02-25 20:21:52 -0500

    added header changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,9 @@ import { Route, Routes } from "react-router-dom";
 
 import Header from "./components/Header";
 import About from "./pages/About";
+import Contact from "./pages/Contact";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
-
-import About from "./pages/About";
-
-import Contact from "./pages/Contact";
-
-import Projects from "./pages/Projects";
-
 import Skills from "./pages/Skills";
 
 function App() {
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,19 +10,13 @@ export default function Header() {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link" href="/about">
-                About
-              </a>
-            </li>
-
-            <li className="nav-item">
-              <Link className="nav-link" aria-current="page" to="/about">
+              <Link className="nav-link" to="/about">
                 About
               </Link>
             </li>
 
             <li className="nav-item">
-              <Link className="nav-link" aria-current="page" to="/projects">
+              <Link className="nav-link" to="/projects">
                 Projects
               </Link>
             </li>
@@ -34,7 +28,7 @@ export default function Header() {
             </li>
 
             <li className="nav-item">
-              <Link className="nav-link" aria-current="page" to="/contact">
+              <Link className="nav-link" to="/contact">
                 Contact
               </Link>
             </li>
