d7c6db6fc0bfd6743d14b6ea8df67dfe00618e56
haneefmhmmd <haneefmhmmd@gmail.com>
2024-02-25 20:32:26 -0500

    updated header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,39 +2,55 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark">
-      <div className="container">
-        <a className="navbar-brand" href="#">
-          IR
-        </a>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <Link className="nav-link" to="/about">
-                About
-              </Link>
-            </li>
+    <>
+      <nav class="navbar navbar-expand-lg navbar-dark">
+        <div class="container">
+          <a class="navbar-brand text-light" href="/">
+            IR
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div
+            class="collapse navbar-collapse justify-content-end"
+            id="navbarSupportedContent"
+          >
+            <ul class="navbar-nav">
+              <li className="nav-item">
+                <Link className="nav-link" to="/about">
+                  About
+                </Link>
+              </li>
 
-            <li className="nav-item">
-              <Link className="nav-link" to="/projects">
-                Projects
-              </Link>
-            </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/projects">
+                  Projects
+                </Link>
+              </li>
 
-            <li className="nav-item">
-              <Link className="nav-link" aria-current="page" to="/gallery">
-                Skills
-              </Link>
-            </li>
+              <li className="nav-item">
+                <Link className="nav-link" aria-current="page" to="/gallery">
+                  Skills
+                </Link>
+              </li>
 
-            <li className="nav-item">
-              <Link className="nav-link" to="/contact">
-                Contact
-              </Link>
-            </li>
-          </ul>
+              <li className="nav-item">
+                <Link className="nav-link" to="/contact">
+                  Contact
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </>
   );
 }
