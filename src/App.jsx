eaf32d0772652f2f969ac5a8921f3c0c982fafc3
Dilshan Nayanamadhu Liyanage <48806985+dilshanliyanage1000@users.noreply.github.com>
2024-02-25 20:18:09 -0500

    Added Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,41 @@
 import "./App.css";
+
+import { Routes, Route } from 'react-router-dom';
+
 import Header from "./components/Header";
+
 import Home from "./pages/Home";
 
+import About from "./pages/About";
+
+import Contact from "./pages/Contact";
+
+import Projects from "./pages/Projects";
+
+import Skills from "./pages/Skills";
+
 function App() {
+
   return (
     <>
       <Header />
-      <Home />
+
+      <Routes>
+
+          <Route exact path='/'element = {<Home/>}> </Route>
+
+          <Route path='/about' element={<About/>}> </Route>
+
+          <Route path='/projects' element = {<Projects/>}> </Route>
+
+          <Route path='/skills' element = {<Skills/>}> </Route>
+
+          <Route path='/contact' element = {<Contact/>}> </Route>
+
+      </Routes>
     </>
   );
+
 }
 
 export default App;
