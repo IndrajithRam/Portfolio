4a5ace9972e9686d5244682926a63e7e0cfe4257
Dilshan Nayanamadhu Liyanage <48806985+dilshanliyanage1000@users.noreply.github.com>
2024-02-25 21:37:00 -0500

    Added Skills

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -6,6 +6,33 @@ export default function Skills() {
 
       <h1 className="display-5 fw-medium" style={{marginTop: "1rem", marginBottom: "3rem", color: "#f04343"}}>Skills</h1>
 
+      <div className="container-fluid">
+                                
+        <div className="col-md-10 mx-auto" style={{ marginTop: "4rem" }}>
+
+          <div className="row" style={{ display: "flex", flexWrap: "wrap", justifyContent: "center", alignItems: "center", marginTop: "2rem"}}>
+            
+            <button type="button" class="btn btn-light skill-pill"> Automation Testing</button>
+            <button type="button" class="btn btn-light skill-pill"> Manual Testing </button>
+            <button type="button" class="btn btn-light skill-pill"> Web Drivers </button>
+            <button type="button" class="btn btn-light skill-pill"> Web Services </button>
+            <button type="button" class="btn btn-light skill-pill"> Selenium </button>
+            <button type="button" class="btn btn-light skill-pill"> Cucumber </button>
+            <button type="button" class="btn btn-light skill-pill"> User Acceptance Testing </button>
+            <button type="button" class="btn btn-light skill-pill"> Functional Testing </button>
+            <button type="button" class="btn btn-light skill-pill"> Soap UI </button>
+            <button type="button" class="btn btn-light skill-pill"> BYNX Tools </button>
+            <button type="button" class="btn btn-light skill-pill"> GitHub </button>
+            <button type="button" class="btn btn-light skill-pill"> Jira </button>
+            <button type="button" class="btn btn-light skill-pill"> REST API </button>
+            <button type="button" class="btn btn-light skill-pill"> Postman </button>
+
+          </div>
+
+        </div>
+            
+      </div>
+
     </main>
 
   ) ;
